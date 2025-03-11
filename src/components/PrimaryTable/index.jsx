bac39ba59dd92f711dc03c96ebbbb62193fa0039
Muhammad Abuzar <mabuzarali1995@gmail.com>
2025-03-11 11:10:15 +0500

    populating data in table

--- src/components/PrimaryTable/index.jsx
+++ src/components/PrimaryTable/index.jsx
@@ -7,8 +7,9 @@ import UpIcon from "../../assets/svg/UpIcon";
 import PendingIcon from "../../assets/svg/PendingIcon";
 import ApprovedIcon from "../../assets/svg/ApprovedIcon";
 import RejectedIcon from "../../assets/svg/RejectedIcon";
+import moment from "moment";
 
-const PrimaryTable = () => {
+const PrimaryTable = ({data}) => {
   const [expandedRowKeys, setExpandedRowKeys] = useState([]);
   const dataSource = [
     {
@@ -79,16 +80,16 @@ const PrimaryTable = () => {
       render: (_, record) => {
         return (
           <div className="flex">
-            <img src={`${record?.img}`} className="rounded" />
+            <img src={`${record?.packedImage}`} className="rounded" width="48px" height="48px" />
             <div className="flex flex-col ml-[12px!important] justify-between">
-              <div className="font-normal text-base">{record?.name}</div>
+              <div className="font-normal text-base">{record?.shippingAddress?.name}</div>
               <div className="flex">
                 <span className="mr-[8px!important] font-medium text-base leading-[100%] tracking-[-2%] underline decoration-solid decoration-[0%] text-[#ff4641] cursor-pointer">
-                  {record?.orderNumber}
+                  {record?.trackingNumber}
                 </span>
-                <span className="font-bold text-[10px] leading-[100%] tracking-[0%] gap-2.5 rounded p-[3px] bg-[#707EFA] text-[white]">
+                {record?.firstOrder&&<span className="font-bold text-[10px] leading-[100%] tracking-[0%] gap-2.5 rounded p-[3px] bg-[#707EFA] text-[white]">
                   New
-                </span>
+                </span>}
               </div>
             </div>
           </div>
@@ -97,30 +98,40 @@ const PrimaryTable = () => {
     },
     {
       title: "Order Date",
-      dataIndex: "date",
-      key: "date",
+      dataIndex: "orderDate",
+      key: "orderDate",
       className: "font-normal text-base leading-[100%] tracking-[-2%]",
+      render:(_,record)=>{
+        return(
+          <span>{moment.unix(record?.orderDate).format("MMM DD, YYYY")}</span>
+        )
+      }
     },
     {
       title: "Status",
-      dataIndex: "status",
-      key: "status",
+      dataIndex: "orderStatus",
+      key: "orderStatus",
       render: (_, record) => {
         return (
           <div>
             <button
               className="flex justify-between items-center px-[10px!important] py-[8px!important] rounded-[100px]"
               style={{
-                background: record?.status === "Pending" ? "#FD7004" :record?.status==='Approved'?"#FF4641":"#A1A1A1",
+                background:
+                  record?.status === "Pending"
+                    ? "#FD7004"
+                    : record?.status === "Approved"
+                    ? "#FF4641"
+                    : "#A1A1A1",
               }}
             >
               <span className="mr-[6px!important]">
-                {record?.status==='Pending'&&<PendingIcon />}
-                {record?.status==='Approved'&&<ApprovedIcon/>}
-                {record?.status==='Rejected'&&<RejectedIcon/>}
+                {record?.status === "Pending" && <PendingIcon />}
+                {record?.status === "Approved" && <ApprovedIcon />}
+                {record?.status === "Rejected" && <RejectedIcon />}
               </span>
               <span className="font-semibold text-sm leading-[100%] tracking-[-2%] text-[#ffffff]">
-                {record.status}
+                {record.orderStatus}
               </span>
             </button>
           </div>
@@ -129,9 +140,14 @@ const PrimaryTable = () => {
     },
     {
       title: "Total Order",
-      dataIndex: "totalOrder",
+      // dataIndex: "totalOrder",
       key: "totalOrder",
       className: "font-normal text-base leading-[100%] tracking-[-2%]",
+      render:(_,record)=>{
+        return(
+          <span>{record?.lineItems?.length}</span>
+        )
+      }
     },
     {
       title: "Total Amount",
@@ -139,52 +155,45 @@ const PrimaryTable = () => {
       render: (_, record) => {
         return (
           <span className="font-bold text-base leading-[100%] tracking-[-2%]">
-            ${record?.totalAmount?.toFixed(2)}
+            ${record?.subTotal?.toFixed(2)}
           </span>
         );
       },
     },
     Table.EXPAND_COLUMN,
-    // {
-    //   title: "",
-    //   key: "actions",
-    //   render: (_, record) => {
-    //     return <button>View</button>;
-    //   },
-    // },
   ];
   return (
     <>
-      <Table
-        dataSource={dataSource}
-        columns={columns}
-        rowKey="key"
-        expandable={{
-          expandedRowKeys,
-          onExpand: (expanded, record) => {
-            setExpandedRowKeys(expanded ? [record.key] : []);
-          },
-          expandedRowRender: (record) => <ExpandableComp />,
-          expandIcon: ({ expanded, onExpand, record }) => (
-            <button
-              onClick={(e) => onExpand(record, e)}
-              className="flex items-center"
-              style={{
-                border: "1px solid #000000",
-                borderRadius: "100px",
-                padding: "8px 13px",
-              }}
-            >
-              <span className="font-bold">View</span>
-              <span className="ml-[8px!important] mt-[2px!important]">
-                {expanded ? <DownIcon /> : <UpIcon />}
-              </span>
-            </button>
-          ),
-        }}
-        footer={null}
-        pagination={false} 
-      />
+        <Table
+          dataSource={data}
+          columns={columns}
+          rowKey="key"
+          expandable={{
+            expandedRowKeys,
+            onExpand: (expanded, record) => {
+              setExpandedRowKeys(expanded ? [record.key] : []);
+            },
+            expandedRowRender: (record) => <ExpandableComp />,
+            expandIcon: ({ expanded, onExpand, record }) => (
+              <button
+                onClick={(e) => onExpand(record, e)}
+                className="flex items-center"
+                style={{
+                  border: "1px solid #000000",
+                  borderRadius: "100px",
+                  padding: "8px 13px",
+                }}
+              >
+                <span className="font-bold">View</span>
+                <span className="ml-[8px!important] mt-[2px!important]">
+                  {expanded ? <DownIcon /> : <UpIcon />}
+                </span>
+              </button>
+            ),
+          }}
+          footer={null}
+          pagination={false}
+        />
     </>
   );
 };
